e1e7952ddb6a346d89f9e18f5e077894d05cdd1e
Martin Hrášek <martin.hrasek@drmax.eu>
2025-04-10 22:38:45 +0200

    Corrections

--- main.tf
+++ main.tf
@@ -4,16 +4,6 @@
  * Terraform building block providing simple management of Azure Application Gateway.
  */
 
-locals {
-  backend_address_pool_name      = "${var.name}-beap"
-  frontend_port_name             = "${var.name}-feport"
-  frontend_ip_configuration_name = "${var.name}-feip"
-  http_setting_name              = "${var.name}-be-htst"
-  listener_name                  = "${var.name}-httplstn"
-  request_routing_rule_name      = "${var.name}-rqrt"
-  redirect_configuration_name    = "${var.name}-rdrcfg"
-}
-
 resource "azurerm_application_gateway" "this" {
   name                = var.name
   location            = var.location != "" ? var.location : data.azurerm_resource_group.this.location
